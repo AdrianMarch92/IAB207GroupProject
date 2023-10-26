2dbf19906ccae020086c56d6ce72a15f380b8560
AdrianMarch92 <115673582+AdrianMarch92@users.noreply.github.com>
2023-10-26 21:52:09 +1000

    made changes to the register and login pages to look a little nicer

--- website/auth.py
+++ website/auth.py
@@ -11,7 +11,7 @@ bp = Blueprint('auth', __name__)
 
 # this is a hint for a login function
 @bp.route('/login', methods=['GET', 'POST'])
-def authenticate(): #view function
+def login():
     print('In Login View function')
     login_form = LoginForm()
     error=None
@@ -27,8 +27,8 @@ def authenticate(): #view function
             login_user(user)
             nextp = request.args.get('next') #this gives the url from where the login page was accessed
             print(nextp)
-            if next is None or not nextp.startswith('/'):
-                return redirect(url_for('index'))
+            if nextp is None or not nextp.startswith('/'):
+                return redirect(url_for('main.index'))
             return redirect(nextp)
         else:
             flash(error)
